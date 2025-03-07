068a645c4e63916aea93c2bb18e84305030e798a
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-03-07 12:12:03 +0800

    Menambahkan jenis kelamin

--- Tugas_pemrograman_desktop/Form1.Designer.vb
+++ Tugas_pemrograman_desktop/Form1.Designer.vb
@@ -31,24 +31,27 @@ Partial Class Form1
         Me.cmbJurusan = New System.Windows.Forms.ComboBox()
         Me.txtNama = New System.Windows.Forms.TextBox()
         Me.txtNIP = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.rablaki = New System.Windows.Forms.RadioButton()
+        Me.rabperempuan = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(87, 61)
+        Me.Label1.Location = New System.Drawing.Point(62, 61)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(77, 32)
+        Me.Label1.Size = New System.Drawing.Size(65, 32)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "NPM"
+        Me.Label1.Text = "NIM"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(87, 289)
+        Me.Label2.Location = New System.Drawing.Point(62, 357)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(116, 32)
         Me.Label2.TabIndex = 1
@@ -59,7 +62,7 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(87, 218)
+        Me.Label3.Location = New System.Drawing.Point(55, 292)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(123, 32)
         Me.Label3.TabIndex = 2
@@ -70,7 +73,7 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(87, 136)
+        Me.Label4.Location = New System.Drawing.Point(62, 136)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(90, 32)
         Me.Label4.TabIndex = 3
@@ -79,7 +82,7 @@ Partial Class Form1
         '
         'btnProcess
         '
-        Me.btnProcess.Location = New System.Drawing.Point(93, 361)
+        Me.btnProcess.Location = New System.Drawing.Point(93, 418)
         Me.btnProcess.Name = "btnProcess"
         Me.btnProcess.Size = New System.Drawing.Size(95, 38)
         Me.btnProcess.TabIndex = 4
@@ -90,39 +93,77 @@ Partial Class Form1
         '
         Me.cmbFakultas.FormattingEnabled = True
         Me.cmbFakultas.Items.AddRange(New Object() {"FMIPA", "FEB", "FKIP"})
-        Me.cmbFakultas.Location = New System.Drawing.Point(295, 222)
+        Me.cmbFakultas.Location = New System.Drawing.Point(224, 292)
         Me.cmbFakultas.Name = "cmbFakultas"
-        Me.cmbFakultas.Size = New System.Drawing.Size(405, 28)
+        Me.cmbFakultas.Size = New System.Drawing.Size(476, 28)
         Me.cmbFakultas.TabIndex = 5
         '
         'cmbJurusan
         '
         Me.cmbJurusan.FormattingEnabled = True
         Me.cmbJurusan.Items.AddRange(New Object() {"Matematika", "Kimia ", "Biologi", "Fisika", "Ilmu Komputer"})
-        Me.cmbJurusan.Location = New System.Drawing.Point(295, 289)
+        Me.cmbJurusan.Location = New System.Drawing.Point(224, 357)
         Me.cmbJurusan.Name = "cmbJurusan"
-        Me.cmbJurusan.Size = New System.Drawing.Size(405, 28)
+        Me.cmbJurusan.Size = New System.Drawing.Size(476, 28)
         Me.cmbJurusan.TabIndex = 6
         '
         'txtNama
         '
-        Me.txtNama.Location = New System.Drawing.Point(295, 143)
+        Me.txtNama.Location = New System.Drawing.Point(224, 143)
         Me.txtNama.Name = "txtNama"
-        Me.txtNama.Size = New System.Drawing.Size(405, 26)
+        Me.txtNama.Size = New System.Drawing.Size(476, 26)
         Me.txtNama.TabIndex = 7
         '
         'txtNIP
         '
-        Me.txtNIP.Location = New System.Drawing.Point(295, 61)
+        Me.txtNIP.Location = New System.Drawing.Point(224, 61)
         Me.txtNIP.Name = "txtNIP"
-        Me.txtNIP.Size = New System.Drawing.Size(405, 26)
+        Me.txtNIP.Size = New System.Drawing.Size(476, 26)
         Me.txtNIP.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(55, 216)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(193, 32)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Jenis Kelamin"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rablaki
+        '
+        Me.rablaki.AutoSize = True
+        Me.rablaki.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rablaki.Location = New System.Drawing.Point(279, 216)
+        Me.rablaki.Name = "rablaki"
+        Me.rablaki.Size = New System.Drawing.Size(155, 36)
+        Me.rablaki.TabIndex = 10
+        Me.rablaki.TabStop = True
+        Me.rablaki.Text = "Laki-Laki"
+        Me.rablaki.UseVisualStyleBackColor = True
+        '
+        'rabperempuan
+        '
+        Me.rabperempuan.AutoSize = True
+        Me.rabperempuan.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rabperempuan.Location = New System.Drawing.Point(464, 216)
+        Me.rabperempuan.Name = "rabperempuan"
+        Me.rabperempuan.Size = New System.Drawing.Size(184, 36)
+        Me.rabperempuan.TabIndex = 11
+        Me.rabperempuan.TabStop = True
+        Me.rabperempuan.Text = "perempuan"
+        Me.rabperempuan.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(969, 468)
+        Me.Controls.Add(Me.rabperempuan)
+        Me.Controls.Add(Me.rablaki)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.txtNIP)
         Me.Controls.Add(Me.txtNama)
         Me.Controls.Add(Me.cmbJurusan)
@@ -147,5 +188,8 @@ Partial Class Form1
     Friend WithEvents cmbJurusan As System.Windows.Forms.ComboBox
     Friend WithEvents txtNama As System.Windows.Forms.TextBox
     Friend WithEvents txtNIP As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents rablaki As System.Windows.Forms.RadioButton
+    Friend WithEvents rabperempuan As System.Windows.Forms.RadioButton
 
 End Class
--- Tugas_pemrograman_desktop/Form1.vb
+++ Tugas_pemrograman_desktop/Form1.vb
@@ -9,10 +9,23 @@
         Dim nama As String = txtNama.Text
         Dim fakultas As String = cmbFakultas.SelectedItem.ToString()
         Dim jurusan As String = cmbJurusan.SelectedItem.ToString()
+        Dim jeniskelamin As String = " "
+
+        If rablaki.Checked Then
+            jeniskelamin = "Laki-Laki"
+        ElseIf rabperempuan.Checked Then
+            jeniskelamin = ""
+        End If
+
 
         MessageBox.Show("Hai : " & nim & vbCrLf &
                         "Nip : " & nama & vbCrLf &
                         "Fakultas : " & fakultas & vbCrLf &
-                        "Jurusan : " & jurusan, "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        "Jurusan : " & jurusan & vbCrLf &
+                        "Jenis Kelamin : " & jeniskelamin, "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
     End Sub
 End Class
