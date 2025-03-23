350e8606e229cc1459801c145a171f73821ba479
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-03-23 17:40:32 +0800

    Tugas terakhir sebelum UTS menambahkan new save delete

--- Tugas_pemrograman_desktop/Tugas3.Designer.vb
+++ Tugas_pemrograman_desktop/Tugas3.Designer.vb
@@ -28,14 +28,14 @@ Partial Class Tugas3
         Me.txtNIP = New System.Windows.Forms.TextBox()
         Me.txtNama = New System.Windows.Forms.TextBox()
         Me.cmbJurusan = New System.Windows.Forms.ComboBox()
-        Me.cmbFakultas = New System.Windows.Forms.ComboBox()
         Me.btnProcess = New System.Windows.Forms.Button()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmbmatkul = New System.Windows.Forms.ComboBox()
+        Me.lblgrade = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
@@ -43,7 +43,21 @@ Partial Class Tugas3
         Me.txtUts = New System.Windows.Forms.TextBox()
         Me.txtUas = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.dgvdata = New System.Windows.Forms.DataGridView()
+        Me.dgnip = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgnama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgjeniskelamin = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgjurusan = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgmatkul = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dggrade = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.btndelete = New System.Windows.Forms.Button()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.btnnew = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
+        CType(Me.dgvdata, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'rabperempuan
@@ -98,26 +112,17 @@ Partial Class Tugas3
         'cmbJurusan
         '
         Me.cmbJurusan.FormattingEnabled = True
-        Me.cmbJurusan.Location = New System.Drawing.Point(261, 327)
+        Me.cmbJurusan.Location = New System.Drawing.Point(219, 249)
         Me.cmbJurusan.Name = "cmbJurusan"
         Me.cmbJurusan.Size = New System.Drawing.Size(476, 28)
         Me.cmbJurusan.TabIndex = 18
         '
-        'cmbFakultas
-        '
-        Me.cmbFakultas.FormattingEnabled = True
-        Me.cmbFakultas.Location = New System.Drawing.Point(261, 262)
-        Me.cmbFakultas.Name = "cmbFakultas"
-        Me.cmbFakultas.Size = New System.Drawing.Size(476, 28)
-        Me.cmbFakultas.TabIndex = 17
-        '
         'btnProcess
         '
-        Me.btnProcess.Location = New System.Drawing.Point(56, 452)
+        Me.btnProcess.Location = New System.Drawing.Point(749, 161)
         Me.btnProcess.Name = "btnProcess"
-        Me.btnProcess.Size = New System.Drawing.Size(95, 38)
+        Me.btnProcess.Size = New System.Drawing.Size(213, 106)
         Me.btnProcess.TabIndex = 16
-        Me.btnProcess.Text = "process"
         Me.btnProcess.UseVisualStyleBackColor = True
         '
         'Label4
@@ -131,22 +136,11 @@ Partial Class Tugas3
         Me.Label4.Text = "Nama"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(92, 262)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(123, 32)
-        Me.Label3.TabIndex = 14
-        Me.Label3.Text = "Fakultas"
-        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(99, 327)
+        Me.Label2.Location = New System.Drawing.Point(50, 249)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(116, 32)
         Me.Label2.TabIndex = 13
@@ -166,33 +160,59 @@ Partial Class Tugas3
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.cmbmatkul)
+        Me.Panel1.Controls.Add(Me.lblgrade)
         Me.Panel1.Controls.Add(Me.Label9)
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.Label7)
         Me.Panel1.Controls.Add(Me.txtTugas)
         Me.Panel1.Controls.Add(Me.txtUts)
         Me.Panel1.Controls.Add(Me.txtUas)
+        Me.Panel1.Controls.Add(Me.cmbJurusan)
         Me.Panel1.Controls.Add(Me.Label6)
         Me.Panel1.Controls.Add(Me.btnProcess)
+        Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Location = New System.Drawing.Point(42, 13)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(934, 516)
+        Me.Panel1.Size = New System.Drawing.Size(1017, 469)
         Me.Panel1.TabIndex = 24
         '
-        'Label10
+        'Label3
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(692, 413)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(86, 20)
-        Me.Label10.TabIndex = 27
-        Me.Label10.Text = "- GRADE -"
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(50, 319)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 32)
+        Me.Label3.TabIndex = 28
+        Me.Label3.Text = "Mata Kuliah"
+        '
+        'cmbmatkul
+        '
+        Me.cmbmatkul.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbmatkul.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbmatkul.FormattingEnabled = True
+        Me.cmbmatkul.Location = New System.Drawing.Point(219, 323)
+        Me.cmbmatkul.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cmbmatkul.Name = "cmbmatkul"
+        Me.cmbmatkul.Size = New System.Drawing.Size(487, 28)
+        Me.cmbmatkul.TabIndex = 29
+        '
+        'lblgrade
+        '
+        Me.lblgrade.AutoSize = True
+        Me.lblgrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblgrade.Location = New System.Drawing.Point(734, 400)
+        Me.lblgrade.Name = "lblgrade"
+        Me.lblgrade.Size = New System.Drawing.Size(121, 25)
+        Me.lblgrade.TabIndex = 27
+        Me.lblgrade.Text = "- GRADE -"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(531, 412)
+        Me.Label9.Location = New System.Drawing.Point(553, 404)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(43, 20)
         Me.Label9.TabIndex = 26
@@ -201,7 +221,7 @@ Partial Class Tugas3
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(364, 412)
+        Me.Label8.Location = New System.Drawing.Point(361, 407)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(41, 20)
         Me.Label8.TabIndex = 25
@@ -210,7 +230,7 @@ Partial Class Tugas3
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(170, 411)
+        Me.Label7.Location = New System.Drawing.Point(145, 401)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(53, 20)
         Me.Label7.TabIndex = 24
@@ -218,7 +238,7 @@ Partial Class Tugas3
         '
         'txtTugas
         '
-        Me.txtTugas.Location = New System.Drawing.Point(229, 409)
+        Me.txtTugas.Location = New System.Drawing.Point(204, 395)
         Me.txtTugas.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtTugas.Name = "txtTugas"
         Me.txtTugas.Size = New System.Drawing.Size(112, 26)
@@ -226,7 +246,7 @@ Partial Class Tugas3
         '
         'txtUts
         '
-        Me.txtUts.Location = New System.Drawing.Point(411, 408)
+        Me.txtUts.Location = New System.Drawing.Point(408, 401)
         Me.txtUts.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtUts.Name = "txtUts"
         Me.txtUts.Size = New System.Drawing.Size(112, 26)
@@ -234,7 +254,7 @@ Partial Class Tugas3
         '
         'txtUas
         '
-        Me.txtUas.Location = New System.Drawing.Point(572, 408)
+        Me.txtUas.Location = New System.Drawing.Point(602, 404)
         Me.txtUas.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtUas.Name = "txtUas"
         Me.txtUas.Size = New System.Drawing.Size(112, 26)
@@ -244,33 +264,144 @@ Partial Class Tugas3
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(65, 403)
+        Me.Label6.Location = New System.Drawing.Point(56, 389)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(62, 29)
         Me.Label6.TabIndex = 20
         Me.Label6.Text = "Nilai"
         '
+        'dgvdata
+        '
+        Me.dgvdata.AllowUserToAddRows = False
+        Me.dgvdata.AllowUserToDeleteRows = False
+        Me.dgvdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvdata.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.dgnip, Me.dgnama, Me.dgjeniskelamin, Me.dgjurusan, Me.dgmatkul, Me.dggrade})
+        Me.dgvdata.Location = New System.Drawing.Point(42, 591)
+        Me.dgvdata.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.dgvdata.Name = "dgvdata"
+        Me.dgvdata.ReadOnly = True
+        Me.dgvdata.RowTemplate.Height = 24
+        Me.dgvdata.Size = New System.Drawing.Size(1008, 184)
+        Me.dgvdata.TabIndex = 26
+        '
+        'dgnip
+        '
+        Me.dgnip.HeaderText = "NIP"
+        Me.dgnip.Name = "dgnip"
+        Me.dgnip.ReadOnly = True
+        '
+        'dgnama
+        '
+        Me.dgnama.HeaderText = "Nama"
+        Me.dgnama.Name = "dgnama"
+        Me.dgnama.ReadOnly = True
+        Me.dgnama.Width = 105
+        '
+        'dgjeniskelamin
+        '
+        Me.dgjeniskelamin.HeaderText = "Jenis Kelamin"
+        Me.dgjeniskelamin.Name = "dgjeniskelamin"
+        Me.dgjeniskelamin.ReadOnly = True
+        Me.dgjeniskelamin.Width = 110
+        '
+        'dgjurusan
+        '
+        Me.dgjurusan.HeaderText = "Jurusan"
+        Me.dgjurusan.Name = "dgjurusan"
+        Me.dgjurusan.ReadOnly = True
+        Me.dgjurusan.Width = 110
+        '
+        'dgmatkul
+        '
+        Me.dgmatkul.HeaderText = "Mata Kuliah"
+        Me.dgmatkul.Name = "dgmatkul"
+        Me.dgmatkul.ReadOnly = True
+        Me.dgmatkul.Width = 110
+        '
+        'dggrade
+        '
+        Me.dggrade.HeaderText = "GRADE"
+        Me.dggrade.Name = "dggrade"
+        Me.dggrade.ReadOnly = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.btnclose)
+        Me.Panel3.Controls.Add(Me.btndelete)
+        Me.Panel3.Controls.Add(Me.btnsave)
+        Me.Panel3.Controls.Add(Me.btnnew)
+        Me.Panel3.Location = New System.Drawing.Point(42, 498)
+        Me.Panel3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1017, 74)
+        Me.Panel3.TabIndex = 25
+        '
+        'btnclose
+        '
+        Me.btnclose.BackColor = System.Drawing.Color.LightCoral
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.Location = New System.Drawing.Point(569, 20)
+        Me.btnclose.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(442, 38)
+        Me.btnclose.TabIndex = 3
+        Me.btnclose.Text = "Close"
+        Me.btnclose.UseVisualStyleBackColor = False
+        '
+        'btndelete
+        '
+        Me.btndelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndelete.Location = New System.Drawing.Point(324, 20)
+        Me.btndelete.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(238, 38)
+        Me.btndelete.TabIndex = 2
+        Me.btndelete.Text = "Delete"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'btnsave
+        '
+        Me.btnsave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsave.Location = New System.Drawing.Point(159, 20)
+        Me.btnsave.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(154, 38)
+        Me.btnsave.TabIndex = 1
+        Me.btnsave.Text = "Save"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'btnnew
+        '
+        Me.btnnew.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnnew.Location = New System.Drawing.Point(3, 20)
+        Me.btnnew.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnnew.Name = "btnnew"
+        Me.btnnew.Size = New System.Drawing.Size(148, 38)
+        Me.btnnew.TabIndex = 0
+        Me.btnnew.Text = "New"
+        Me.btnnew.UseVisualStyleBackColor = True
+        '
         'Tugas3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1073, 530)
+        Me.ClientSize = New System.Drawing.Size(1224, 784)
+        Me.Controls.Add(Me.dgvdata)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.rabperempuan)
         Me.Controls.Add(Me.rablaki)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.txtNIP)
         Me.Controls.Add(Me.txtNama)
-        Me.Controls.Add(Me.cmbJurusan)
-        Me.Controls.Add(Me.cmbFakultas)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Tugas3"
         Me.Text = "Tugas3"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.dgvdata, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -281,14 +412,11 @@ Partial Class Tugas3
     Friend WithEvents txtNIP As System.Windows.Forms.TextBox
     Friend WithEvents txtNama As System.Windows.Forms.TextBox
     Friend WithEvents cmbJurusan As System.Windows.Forms.ComboBox
-    Friend WithEvents cmbFakultas As System.Windows.Forms.ComboBox
     Friend WithEvents btnProcess As System.Windows.Forms.Button
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
-    Friend WithEvents Label10 As System.Windows.Forms.Label
     Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
@@ -296,4 +424,19 @@ Partial Class Tugas3
     Friend WithEvents txtUts As System.Windows.Forms.TextBox
     Friend WithEvents txtUas As System.Windows.Forms.TextBox
     Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents dgvdata As System.Windows.Forms.DataGridView
+    Friend WithEvents dgnip As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgnama As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgjeniskelamin As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgjurusan As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgmatkul As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dggrade As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents btnclose As System.Windows.Forms.Button
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+    Friend WithEvents btnsave As System.Windows.Forms.Button
+    Friend WithEvents btnnew As System.Windows.Forms.Button
+    Friend WithEvents lblgrade As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents cmbmatkul As System.Windows.Forms.ComboBox
 End Class
--- Tugas_pemrograman_desktop/Tugas3.vb
+++ Tugas_pemrograman_desktop/Tugas3.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Tugas3
 
     Private Sub Tugas3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cmbFakultas.Items.Add("FMIPA")
-        cmbFakultas.Items.Add("FEB")
-        cmbFakultas.Items.Add("FKIP")
+        cmbJurusan.Items.Add("Manajemen Informatika")
+        cmbJurusan.Items.Add("Ilmu Komputer")
+        cmbJurusan.Items.Add("Sistem Informasi")
     End Sub
 
     Private Sub txtNIP_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNIP.KeyPress
@@ -20,7 +20,6 @@
             Exit Sub
         End If
         Dim nama As String = txtNama.Text.Trim()
-        Dim fakultas As String = cmbFakultas.Text.Trim()
         Dim jurusan As String = cmbJurusan.Text.Trim()
         Dim tugas As Double = Double.Parse(txtTugas.Text)
         Dim uts As Double = Double.Parse(txtUts.Text)
@@ -55,7 +54,6 @@
         MessageBox.Show("Hai : " & nama & vbCrLf &
                         "NIP : " & nip & vbCrLf &
                         "Jenis Kelamin : " & jenisKelamin & vbCrLf &
-                        "Fakultas : " & fakultas & vbCrLf &
                         "Jurusan : " & jurusan & vbCrLf &
                         "Nilai Akhir: " & total.ToString() & vbCrLf &
                         "GRADE: " & grade,
@@ -64,26 +62,152 @@
                         MessageBoxIcon.Information)
     End Sub
 
-    Private Sub cmbFakultas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbFakultas.SelectedIndexChanged
-        Dim fakultas As String = cmbFakultas.SelectedItem
-        cmbJurusan.Items.Clear()
-        Select Case (fakultas)
-            Case "FMIPA"
-                cmbJurusan.Items.Add("Matematika")
-                cmbJurusan.Items.Add("Kimia")
-                cmbJurusan.Items.Add("Biologi")
-                cmbJurusan.Items.Add("Fisika")
-                cmbJurusan.Items.Add("Ilmu Komputer")
-            Case "FKIP"
-                cmbJurusan.Items.Add("Pendidikan Teknologi Informasi")
-                cmbJurusan.Items.Add("Pendidikan Fisika")
-                cmbJurusan.Items.Add("Pendidikan Kimia")
-                cmbJurusan.Items.Add("Pendidikan Bahasa Indonesia")
-                cmbJurusan.Items.Add("Pendidikan Bahasa Inggris")
-            Case "FEB"
-                cmbJurusan.Items.Add("Akuntansi")
-                cmbJurusan.Items.Add("Manajemen")
-                cmbJurusan.Items.Add("Ekonomi Pembangunan")
+    Private Sub cmbFakultas_SelectedIndexChanged(sender As Object, e As EventArgs)
+    End Sub
+
+
+    Private Sub btnnew_Click(sender As Object, e As EventArgs) Handles btnnew.Click
+        txtNIP.Clear()
+        txtNama.Clear()
+        cmbJurusan.SelectedIndex = -1
+        cmbmatkul.SelectedIndex = -1
+        txtTugas.Clear()
+        txtUts.Clear()
+        txtUas.Clear()
+        lblgrade.Text = " -GRADE- "
+        txtNIP.Enabled = True
+        txtNIP.Focus()
+        rablaki.Checked = False
+        rabperempuan.Checked = False
+    End Sub
+
+    Private Sub cmbJurusan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbJurusan.SelectedIndexChanged
+        Dim fakultas As String = cmbJurusan.SelectedItem
+        cmbmatkul.Items.Clear()
+        Select (fakultas)
+            Case "Manajemen Informatika"
+                cmbmatkul.Items.Add("Pemrograman Desktop")
+                cmbmatkul.Items.Add("Pemrograman WEB Lanjut")
+                cmbmatkul.Items.Add("Keamanan Sistem Informasi")
+            Case "Ilmu Komputer"
+                cmbmatkul.Items.Add("Kecerdasan Buatan")
+                cmbmatkul.Items.Add("Aljabar Linear")
+            Case "Sistem Informasi"
+                cmbmatkul.Items.Add("Pemrograman Web")
+                cmbmatkul.Items.Add("PBO")
         End Select
+    End Sub
+
+    Private Sub dgvdata_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvdata.CellContentClick
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex >= 0 Then
+            Dim row As DataGridViewRow = dgvdata.Rows(e.RowIndex)
+
+            txtNIP.Text = row.Cells("dgnip").Value.ToString()
+            txtNama.Text = row.Cells("dgnama").Value.ToString()
+
+            If row.Cells("dgjeniskelamin").Value.ToString() = "Laki-Laki" Then
+                rablaki.Checked = True
+            Else
+                rabperempuan.Checked = True
+            End If
+
+            cmbJurusan.Text = row.Cells("dgjurusan").Value.ToString()
+            cmbmatkul.Text = row.Cells("dgmatkul").Value.ToString()
+            lblgrade.Text = row.Cells("dggrade").Value.ToString()
+
+            txtNIP.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
+        If txtNIP.Text = "" Or txtNama.Text = "" Or cmbmatkul.Text = "" Or (Not rablaki.Checked And Not rabperempuan.Checked) Then
+            MessageBox.Show("Harap isi semua data!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim rowIndex As Integer = -1
+        For Each row As DataGridViewRow In dgvdata.Rows
+            If row.Cells("dgnip").Value IsNot Nothing AndAlso row.Cells("dgnip").Value.ToString() = txtNIP.Text Then
+                rowIndex = row.Index
+                Exit For
+            End If
+        Next
+
+        If rowIndex = -1 Then
+            dgvdata.Rows.Add(txtNIP.Text, txtNama.Text, If(rablaki.Checked, "Laki-Laki", "Perempuan"), cmbJurusan.Text, cmbJurusan.Text, lblgrade.Text)
+        Else
+            dgvdata.Rows(rowIndex).Cells("dgnip").Value = txtNIP.Text
+            dgvdata.Rows(rowIndex).Cells("dgnama").Value = txtNama.Text
+            dgvdata.Rows(rowIndex).Cells("dgjeniskelamin").Value = If(rablaki.Checked, "Laki-Laki", "Perempuan")
+            dgvdata.Rows(rowIndex).Cells("dgjurusan").Value = cmbJurusan.Text
+            dgvdata.Rows(rowIndex).Cells("dgmatkul").Value = cmbmatkul.Text
+            dgvdata.Rows(rowIndex).Cells("dggrade").Value = lblgrade.Text
+        End If
+        btnnew.PerformClick()
+    End Sub
+
+    Private Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        If dgvdata.SelectedRows.Count > 0 Then
+            Dim confirm As DialogResult = MessageBox.Show("Apakah yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If confirm = DialogResult.Yes Then
+                dgvdata.Rows.RemoveAt(dgvdata.SelectedRows(0).Index)
+                btnnew.PerformClick()
+            End If
+        Else
+            MessageBox.Show("Pilih data yang ingin dihapus!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtTugas_TextChanged(sender As Object, e As EventArgs) Handles txtTugas.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub UpdateGrade()
+        Dim tugas, uts, uas As Double
+        If Double.TryParse(txtTugas.Text, tugas) AndAlso Double.TryParse(txtUts.Text, uts) AndAlso Double.TryParse(txtUas.Text, uas) Then
+            Dim total As Double = (tugas * 0.3) + (uts * 0.3) + (uas * 0.4)
+            Dim grade As String
+
+            Select Case total
+                Case Is >= 78
+                    grade = "A"
+                Case Is >= 65
+                    grade = "B"
+                Case Is >= 50
+                    grade = "C"
+                Case Is >= 40
+                    grade = "D"
+                Case Else
+                    grade = "E"
+            End Select
+
+            lblgrade.Text = " " & grade
+        Else
+            lblgrade.Text = " -GRADE- "
+        End If
+    End Sub
+
+    Private Sub txtUts_TextChanged(sender As Object, e As EventArgs) Handles txtUts.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub txtUas_TextChanged(sender As Object, e As EventArgs) Handles txtUas.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub lblgrade_Click(sender As Object, e As EventArgs) Handles lblgrade.Click
+
+    End Sub
+
+    Private Sub cmbmatkul_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbmatkul.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub dgvdata_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvdata.CellContentDoubleClick
+
     End Sub
 End Class
